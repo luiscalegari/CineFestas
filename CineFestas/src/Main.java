f1541dd49e83edf63b43932b5c27dcdf122e1fdd
Aluno <Aluno@LS02M08>
2015-05-07 19:36:26 -0300

    CineFesta - Icone

---
+++ CineFestas/src/Main.java
@@ -0,0 +1,26 @@
+
+import engenharia.cine.festa.util.MensagensUtil;
+import engenharia.cine.festas.gui.CadClienteFrame;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().executa();
+    }
+
+    private void executa() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+            MensagensUtil.addMsg(null, "Não foi possível iniciar o sistema!!!");
+        }
+        
+        new CadClienteFrame().setVisible(true);
+    }
+}
